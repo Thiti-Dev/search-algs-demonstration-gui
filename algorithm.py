5e7b661fdb1a810a6581c26bd0d25412c4d0a736
Thiti Dev <thiti.mwk.main@gmail.com>
2020-12-18 19:23:36 +0700

    Created (Class)Dijkstra

--- algorithm.py
+++ algorithm.py
@@ -75,3 +75,40 @@ class Kruskal:
             total_mst = total_mst+weight
 
         return orderized_list,total_mst
+
+class Dijkstra:
+
+    def __init__(self, vertices, graph):
+        self.vertices = vertices  # ("A", "B", "C" ...)
+        self.graph = graph  # {"A": {"B": 1}, "B": {"A": 3, "C": 5} ...}
+
+    def find_route(self, start, end):
+        unvisited = {n: float("inf") for n in self.vertices}
+        unvisited[start] = 0  # set start vertex to 0
+        visited = {}  # list of all visited nodes
+        parents = {}  # predecessors
+        while unvisited:
+            min_vertex = min(unvisited, key=unvisited.get)  # get smallest distance
+            for neighbour, _ in self.graph.get(min_vertex, {}).items():
+                if neighbour in visited:
+                    continue
+                print(f'currently considering: {min_vertex} -> {neighbour}')
+                new_distance = unvisited[min_vertex] + self.graph[min_vertex].get(neighbour, float("inf"))
+                if new_distance < unvisited[neighbour]:
+                    unvisited[neighbour] = new_distance
+                    parents[neighbour] = min_vertex
+            visited[min_vertex] = unvisited[min_vertex]
+            unvisited.pop(min_vertex)
+            if min_vertex == end:
+                break
+        return parents, visited
+
+    @staticmethod
+    def generate_path(parents, start, end):
+        path = [end]
+        while True:
+            key = parents[path[0]]
+            path.insert(0, key)
+            if key == start:
+                break
+        return path
